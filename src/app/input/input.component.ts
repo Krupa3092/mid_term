8bcb762758ad4802832044b0496b321ad41e41fb
Krupa Pravinsinh Rajput <rajpukru@sheridancollege.ca>
2024-02-21 10:29:14 -0500

    final

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   standalone: true,
+  imports: [
+    NgForOf,
+    FormsModule
+  ],
   styleUrls: ['./input.component.css']
 })
 export class InputComponent {
